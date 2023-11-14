72a420f7207992aee7803561c4f61c0c5480740a
alanpaulb <alanpaulb2005@gmail.com>
2023-11-15 02:29:56 +0400

    Create exercise 1.py

---
+++ Chapter 5- Dictionaries/exercise 1.py
@@ -0,0 +1,13 @@
+#Create a dictionary to store information about a person i know
+person_info = {
+    "first_name": "Alan",
+    "last_name": "Paul",
+    "age": 18,
+    "city": "Kollam"
+}
+
+#Print each piece of information stored in the dictionary
+print("First Name:", person_info["first_name"])
+print("Last Name:", person_info["last_name"])
+print("Age:", person_info["age"])
+print("City:", person_info["city"])
